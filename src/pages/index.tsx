f44d20c4f6556a787b874fc7dbb6f7f21c394fbe
Pedro Freitas <pedrinfreitas@gmail.com>
2021-11-17 21:56:09 -0300

    feat: getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import type { GetServerSideProps } from 'next';
+import type { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -41,7 +41,7 @@ export default function Home({ product }: IHomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1JwaUKCwtMwOQa5XHai7y7Ap', /*{
     expand: ['product']
   }*/)
@@ -58,6 +58,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 horas
   }
 }
